a0f52baedcb2583891a5ae2d81b4fe7c63343df9
Daniel Schnöll <daniel.schnoell@tuwien.ac.at>
2025-08-12 15:50:30 +0200

    updated setup to also check for python devel

--- setup.py
+++ setup.py
@@ -9,10 +9,25 @@ except KeyError:
 
 
 import sys
+import os
 import subprocess
+import sysconfig
+
+# check if python-devel is installed
+if not os.path.exists(os.path.join(sysconfig.get_config_var('INCLUDEPY'), 'Python.h')):
+    print("ERROR: Python development headers not found. Please install python3-devel or python3-dev package.",file=sys.stderr)
+    print("ERROR: On Ubuntu/Debian: sudo apt install python3-dev",file=sys.stderr)
+    print("ERROR: On Fedora/RHEL: sudo dnf install python3-devel",file=sys.stderr)
+    print("ERROR: Required for building the C++ part of the project. Mannually disable this check if you are not building the C++ part.",file=sys.stderr)
+    sys.exit()
+else:
+    print("Python development headers found. Proceeding with the installation.")
+
+
 
 # Upgrade pip
 subprocess.check_call(['git', 'submodule', 'init'])
+subprocess.check_call(['git', 'submodule', 'update'])
 
 # Upgrade pip
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
